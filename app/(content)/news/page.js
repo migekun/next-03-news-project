31deab49d39bf772c55b24e1976e43c8b21a6fd5
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-21 17:41:56 +0900

    Refactorizar para obtener los datos de la base de datos directamente sin pasar por el 'proyecto' backend

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,44 +1,12 @@
-'use client';
-
-import {useEffect, useState } from 'react';
 import NewsList from '@/components/news-list';
+import {getAllNews} from "@/lib/news";
 
-export default function NewsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState()
-  const [news, setNews] = useState([])
-
-  useEffect(() => {
-    alert('useEffect')
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-      if (!response.ok) {
-        setError('Could not fetch news.');
-        setIsLoading(false);
-      }
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
-  if (error)  {
-    return <p>{error}</p>
-  }
-
-  let newsContext;
-  if (news) {
-    newsContext = <NewsList news={news} />
-  }
+export default async function NewsPage() {
+  const news = getAllNews()
   return (
     <>
       <h1>News Page</h1>
-      {newsContext}
+      <NewsList news={news} />
     </>
   );
 }
